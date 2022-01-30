3b611ef4f44128c282431233b2f4fc4870c4e230
Viorel Pacurar <viorelioan.pacurar@live.com>
2022-01-30 11:52:01 +0200

    Add project files.

---
+++ FoodOrder/Services/UserService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+using System.Threading.Tasks;
+using System.Linq;
+using FoodOrder.Model;
+using Firebase.Database.Query;
+
+namespace FoodOrder.Services
+{
+    public class UserService
+    {
+        FirebaseClient client;
+
+        public UserService()
+        {
+            client = new FirebaseClient("https://foodorderapp-da474-default-rtdb.firebaseio.com/");
+        }
+
+        public async Task<bool> IsUserExists(string uname) //Check if the user exists
+        {
+            var user = (await client.Child("Users")  //Users will act as table
+                .OnceAsync<User>()).Where(u => u.Object.Username == uname).FirstOrDefault();
+
+            return (user != null);
+        }
+
+        public async Task<bool> RegisterUser(string uname, string passwd)
+        {
+            if (await IsUserExists(uname) == false)
+            {
+                await client.Child("Users")
+                .PostAsync(new User()
+                {
+                    Username = uname,
+                    Password = passwd
+                });
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public async Task<bool> LoginUser(string uname, string passwd) //Check if the user/pass combination exists
+        {
+            var user = (await client.Child("Users")  //Users will act as table
+                .OnceAsync<User>()).Where(u => u.Object.Username == uname).Where(u => u.Object.Password == passwd).FirstOrDefault();
+
+            return (user != null);
+        }
+
+
+    }
+}
---
+++ FoodOrder/ViewModels/LoginViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FoodOrder.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private string _Username;
+        private string Username;
+
+        private string _Password;
+        private string Passwoed;
+
+        private bool _IsBusy;
+        private bool IsBusy;
+
+        public Command LoginCommand { get; set; }
+        public Command RegisterCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(async () => await LoginCommandAsync());
+            RegisterCommand = new Command(async () => await RegisterCommandAsync());
+        }
+
+        private Task RegisterCommandAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task LoginCommandAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
